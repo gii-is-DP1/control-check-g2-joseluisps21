43f200f2dd4033a7a52bef6423ac4335a3870d0a
joseluisps21 <joseluissfc4@gmail.com>
2022-01-14 09:51:47 +0100

    entrega examen lab

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
@@ -2,9 +2,18 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FeedingService {
+
+    @Autowired
+    private FeedingRepository feedingRepository;
+
     public List<Feeding> getAll(){
-        return null;
+        List<Feeding> f = feedingRepository.findAll();
+        return f;
     }
 
     public List<FeedingType> getAllFeedingTypes(){
@@ -12,7 +21,8 @@ public class FeedingService {
     }
 
     public FeedingType getFeedingType(String typeName) {
-        return null;
+        FeedingType tipoFeeding = feedingRepository.findFeedingType(typeName);
+        return tipoFeeding;
     }
 
     public Feeding save(Feeding p) throws UnfeasibleFeedingException {
